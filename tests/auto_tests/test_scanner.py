251258699f1a8e8cb1822f82a2bb49fa09c77ad0
roy <endvroy@gmail.com>
2017-07-03 22:45:59 -0700

    scanner tests updated

--- tests/auto_tests/test_scanner.py
+++ tests/auto_tests/test_scanner.py
@@ -27,7 +27,8 @@ class TestScanner(unittest.TestCase):
         category_info = {0: CategoryInfo('register', 0),
                          1: CategoryInfo('if', -1),
                          2: CategoryInfo('id', -2),
-                         3: CategoryInfo('blank', -3)}
+                         3: CategoryInfo('blank', -3),
+                         4: CategoryInfo('eq', -4)}
 
         d = alt(atom(str(i), 0) for i in range(10))
         nfa0 = cat([atom('r', -1), d, d])
@@ -36,15 +37,18 @@ class TestScanner(unittest.TestCase):
         nfa2 = cat([w, closure(w)])
         space = atom(' ', 3)
         nfa3 = cat([space, closure(space)])
-        nfa = alt([nfa0, nfa1, nfa2, nfa3])
+        nfa4 = atom('=', 4)
+        nfa = alt([nfa0, nfa1, nfa2, nfa3, nfa4])
         dfa = nfa_to_dfa(nfa, category_info)
         min_dfa = minimize_dfa(dfa)
         scanner = Scanner(min_dfa, category_info)
-        tokens = scanner.tokens('if  var r31')
+        tokens = scanner.tokens('if  var = r31')
         self.assertEqual(list(tokens),
                          [(1, 'if'),
                           (3, '  '),
                           (2, 'var'),
+                          (3, ' '),
+                          (4, '='),
                           (3, ' '),
                           (0, 'r31')])
 
