64e5f9f9e49182e3c55cbc6ab7ee142ebfe73aa0
roy <endvroy@gmail.com>
2017-07-27 17:16:31 -0700

    calc LALR parse table completed

--- lalr_parser/rule_set.py
+++ lalr_parser/rule_set.py
@@ -1,22 +1,13 @@
 from parserlib.rule_set import RuleSet, d, t, nt
 from metachar import eof
+from enum import Enum, unique
 
 
-class Item:
-    def __init__(self, ntid, rule_id, pos, la_set):
-        self.ntid = ntid
-        self.rule_id = rule_id
-        self.pos = pos
-        self.la_set = frozenset(la_set)
-
-    def __eq__(self, other):
-        return self.ntid == other.ntid and self.rule_id == other.rule_id and self.pos == other.pos and self.la_set == other.la_set
-
-    def __hash__(self):
-        return hash(self.ntid) ^ hash(self.rule_id) ^ hash(self.pos) ^ hash(self.la_set)
-
-    def __repr__(self):
-        return f'Item({self.ntid}, {self.rule_id}, {self.pos}, {self.la_set})'
+@unique
+class Action(Enum):
+    shift = 0
+    reduce = 1
+    accept = 2
 
 
 class LALRRuleSet(RuleSet):
@@ -68,3 +59,53 @@ class LALRRuleSet(RuleSet):
             ntid, rule_id, pos = item
             new_set[(ntid, rule_id, pos + 1)] = la_set.copy()
         return new_set
+
+    def calc_parse_table(self):
+        initial = {(self.goal, i, 0): {eof} for i in range(len(self.nt_rules[self.goal]))}
+        initial = self.item_closure(initial)
+        cc = [initial]
+        cc_map = {frozenset(initial.keys()): 0}
+
+        action = []
+        goto = []
+
+        # build cc and goto table
+        i = 0
+        while i < len(cc):
+            item_set = cc[i]
+            goto.append({})
+
+            partition = self.item_partition_goto(item_set)
+            for sym, item_set_x in partition.items():
+                new_item_set = self.item_closure(self.item_advance(item_set_x))
+                cc_key = frozenset(new_item_set.keys())
+
+                # update canonical collection
+                if cc_key in cc_map:
+                    for k, v in new_item_set.items():
+                        cc[cc_map[cc_key]][k] |= v  # merge lookahead
+                else:  # found new state
+                    cc_map[cc_key] = len(cc)
+                    cc.append(new_item_set)
+
+                goto_id = cc_map[cc_key]
+                goto[i][sym] = goto_id  # update goto table
+            i += 1
+
+        for i, item_set in enumerate(cc):  # build action table
+            action.append({})
+            for item, la_set in item_set.items():
+                ntid, rule_id, pos = item
+                derives = self.nt_rules[ntid][rule_id]  # fetch the original rule
+                for category in la_set:
+                    if pos == len(derives):
+                        if category == eof and ntid == self.goal:
+                            action[i][eof] = Action.accept,  # accept
+                        else:
+                            action[i][category] = Action.reduce, ntid, rule_id  # reduce
+                    else:
+                        next_sym = derives[pos]
+                        if next_sym[0] == 't':
+                            action[i][next_sym[1]] = Action.shift,  # shift
+
+        return action, goto  # action table, goto table
--- lalr_parser/tests/test_rule_set.py
+++ lalr_parser/tests/test_rule_set.py
@@ -1,5 +1,6 @@
 import unittest
 from lalr_parser.rule_set import *
+import itertools
 
 
 def build_test_rule():
@@ -86,6 +87,78 @@ class TestRuleSet(unittest.TestCase):
         result = rule_set.item_advance(item_set)
         self.assertEqual(result, {(pair, 0, 1): {')'},
                                   (pair, 1, 1): {')'}})
+
+    def assertIsomorphism(self, action, goto, correct_action, correct_goto):
+        if len(action) != len(correct_action) or len(goto) != len(correct_goto):
+            self.fail(f'number of states unequal: correct = {len(correct_action)}, got = {len(action)}')
+
+        for state_map in itertools.permutations(range(len(action))):
+            mapped_action = [action[i] for i in state_map]
+            mapped_goto = [{} for i in range(len(action))]
+            for a, b in enumerate(state_map):
+                mapped_goto[a] = goto[b].copy()
+                for k, v in mapped_goto[a].items():
+                    mapped_goto[a][k] = state_map[v]
+            if mapped_action == correct_action and mapped_goto == correct_goto:
+                return
+        else:
+            self.fail('parse tables non-isomorphic')
+
+    def test_calc_parse_table(self):
+        rule_set = LALRRuleSet()  # example grammar on EC pp.120
+        goal, list_, pair = rule_set.new_nt(3)
+        rule_set.add_rule(goal, d(nt(list_)))
+        rule_set.add_rule(list_, d(nt(list_), nt(pair)))
+        rule_set.add_rule(list_, d(nt(pair)))
+        rule_set.add_rule(pair, d(t('('), nt(pair), t(')')))
+        rule_set.add_rule(pair, d(t('('), t(')')))
+        rule_set.mark_goal(goal)
+
+        action, goto = rule_set.calc_parse_table()
+        correct_action = [{'(': (Action.shift,)},
+                          {'(': (Action.shift,), eof: (Action.accept,)},
+                          {'(': (Action.reduce, 1, 1), eof: (Action.reduce, 1, 1)},
+                          {'(': (Action.shift,), ')': (Action.shift,)},
+                          {'(': (Action.reduce, 1, 0), eof: (Action.reduce, 1, 0)},
+                          {')': (Action.shift,)},
+                          {'(': (Action.reduce, 2, 1), eof: (Action.reduce, 2, 1)},
+                          {'(': (Action.reduce, 2, 0), eof: (Action.reduce, 2, 0)}]
+
+        correct_goto = [{('nt', 1): 1, ('nt', 2): 2, ('t', '('): 3},
+                        {('nt', 2): 4, ('t', '('): 3},
+                        {},
+                        {('nt', 2): 5, ('t', '('): 3, ('t', ')'): 6},
+                        {},
+                        {('t', ')'): 7},
+                        {},
+                        {}]
+
+        self.assertIsomorphism(action, goto, correct_action, correct_goto)
+
+    def test_calc_parse_table_2(self):
+        rule_set = LALRRuleSet()  # example grammar on stanford handout pp.0
+        S, X = rule_set.new_nt(2)
+        rule_set.add_rule(S, d(nt(X), nt(X)))
+        rule_set.add_rule(X, d(t('a'), nt(X)))
+        rule_set.add_rule(X, d(t('b')))
+        rule_set.mark_goal(S)
+
+        action, goto = rule_set.calc_parse_table()
+        correct_action = [{'a': (Action.shift,), 'b': (Action.shift,)},
+                          {'a': (Action.shift,), 'b': (Action.shift,)},
+                          {'a': (Action.shift,), 'b': (Action.shift,)},
+                          {'a': (Action.reduce, 1, 1), 'b': (Action.reduce, 1, 1), eof: (Action.reduce, 1, 1)},
+                          {eof: (Action.accept,)},
+                          {'a': (Action.reduce, 1, 0), 'b': (Action.reduce, 1, 0), eof: (Action.reduce, 1, 0)}]
+
+        correct_goto = [{('nt', X): 1, ('t', 'a'): 2, ('t', 'b'): 3},
+                        {('nt', X): 4, ('t', 'a'): 2, ('t', 'b'): 3},
+                        {('nt', X): 5, ('t', 'a'): 2, ('t', 'b'): 3},
+                        {},
+                        {},
+                        {}]
+
+        self.assertIsomorphism(action, goto, correct_action, correct_goto)
 
 
 if __name__ == '__main__':
